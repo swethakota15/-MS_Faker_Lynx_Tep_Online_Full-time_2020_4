a10637200e761d2812a3d51737dfb490c4324c9c
vara <gadhirajuvaraprasad@gmail.com>
2020-11-14 11:05:48 +0530

    added a sub-cat

--- run.js
+++ run.js
@@ -3,4 +3,5 @@ var kkr=require("../fake")
 console.log(kkr.satellites.countries());
 console.log(kkr.satellites.numbersat());
 console.log(kkr.satellites.operationaldate());
+console.log(kkr.satellites.location());
 
